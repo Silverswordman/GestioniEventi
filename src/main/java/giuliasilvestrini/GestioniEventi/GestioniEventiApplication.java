95bfd1f5cedbc988513a07a3f12ae8aafdf5ba7a
Giulia Silvestrini <silvergiuly@hotmail.it>
2024-01-26 10:17:04 +0100

    Initial commit

---
+++ src/main/java/giuliasilvestrini/GestioniEventi/GestioniEventiApplication.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.GestioniEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioniEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioniEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuliasilvestrini/GestioniEventi/GestioniEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.GestioniEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioniEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
